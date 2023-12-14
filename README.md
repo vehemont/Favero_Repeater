17f0708ebb2ce2da6c37acdb2b0d0e436f62ed52
Brad <48965163+vehemont@users.noreply.github.com>
2023-12-14 09:39:55 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,57 @@
+# Favero_Repeater
+
+The Favero Full Arm scoring machines use serial data output that can be interpreted through an Arduino and then used to send data signals for ws2812b LED lights for strip repeater lights.
+
+This uses one [Wemos D1 mini](https://www.amazon.com/Organizer-ESP8266-Internet-Development-Compatible/dp/B081PX9YFV/ref=sr_1_3?crid=3HISM104Q8NMO&keywords=wemos+d1+mini&qid=1636054524&qsid=133-6299387-8119633&sprefix=wemos%2Caps%2C400&sr=8-3&sres=B081PX9YFV%2CB08C7FYM5T%2CB08QZ2887K%2CB07W8ZQY62%2CB07RBNJLK4%2CB08FZ9YDGS%2CB07BK435ZW%2CB07V84VWSM%2CB07PF3NK12%2CB08H1YRN4M%2CB08FQYZX37%2CB073CQVFLK%2CB07VN328FS%2CB0899N647N%2CB07G9HZ5LM%2CB08MKLRSNH&srpt=SINGLE_BOARD_COMPUTER). The hardware is assembled as shown in the [schematic](Schematic_Favero_Overlay.pdf) under "Favero Serial Parser". You do not need to worry about anything else in the schematic, unless you are setting up https://github.com/BenKohn2004/Favero_Overlay. 
+
+
+<h2>Topology</h2>
+
+<h3>Requirements</h3>
+
+- Two ws2812 RGB LED strips and knowing how many total LEDs in each strip to change line 11 in [Wemos_Favero_Serial_Parser_Rev_1.ino](Wemos_Favero_Serial_Parser_Rev_1/Wemos_Favero_Serial_Parser_Rev_1.ino).  
+- Power source for the RGB LED strips and microcontroller (not covered here)
+- A 100 ohm resistor
+- A favero full arm scoring machine with a rj-11 serial port on the back.
+- A microcontroller: I am using a Wemo D1 mini in this example
+- An RJ11 cable to connect from your Favero scoring machine to your microcontroller. An easy option is to use a RJ11 breakout such as [this](https://a.co/d/c3JmIds).
+
+<h3>Microcontroller to Favero</h3>
+
+The section in the [schematic](Schematic_Favero_Overlay.pdf) for "Favero Serial Parser" is what is required for a serial connection from the microcontroller to the Favero scoring machine.
+
+- Plug one end of an RJ-11 cable into the Favero FA scoring machine serial port on the back (keep powered off completely until ready to test).
+- Wemo 3.3V pin to Favero RJ-11 pin 1.
+- Wemo RX pin to Favero RJ-11 pin 2.
+- Wemo GND pin to Favero RJ-11 pin 3.
+    - There must be a 100 Ohm resistor in between the Favero scoring machine and GND (no other devices need the 100 ohm resistor).
+    - The GND pin on the Wemo will also need to be connected to your LED strips and their power source ground as well.
+
+<h3>Microcontroller to LEDs</h3>
+
+- Connect your "left fencer" LED strip data to D2 (pin 4) (fencer on the left / FOTL) on the Wemo.
+- Connect the "right fencer" LED strip data to D1 (pin 5) (fencer on the right / FOTR) on the Wemo.
+- The GND pins will need to be connected on the same rail to your power source, LED strip, the scoring machine, and the Wemo's ground pin. A 100 ohm resistor is required from GND to the Favera FA scoring machine RJ-11 pin 3 (no other devices).
+- Connect your 5v power to each LED strip. 
+
+
+<h3>Edit  Wemos_Favero_Serial_Parser_Rev_1.ino</h3>
+
+- Edit line 10 of [Wemos_Favero_Serial_Parser_Rev_1.ino](Wemos_Favero_Serial_Parser_Rev_1/Wemos_Favero_Serial_Parser_Rev_1.ino#L10) to the total number of LEDs (applies to both fencers).
+- Edit line 44 of [Wemos_Favero_Serial_Parser_Rev_1.ino](Wemos_Favero_Serial_Parser_Rev_1/Wemos_Favero_Serial_Parser_Rev_1.ino#L44) to the power draw you want. This will cap FastLED to only output a maximum number of volts and milliamps to prevent your LEDs from going out. 
+    - `FastLED.setMaxPowerInVoltsAndMilliamps(5, 2000)` - Where `5` is 5 volts, and `2000` is 2000 milliamps (2 amps). This applies to each LED matrix seperately.
+
+<h3>Troubleshooting</h3>
+
+I ran into an issue with my power supply for my LEDs causing lots of noise. I would see random serial output that didn't make sense because the scoring machine was having no changes, but I was receiving serial output for changes. Unplugging the LED power resolved the noise issue. I changed power supplies for my LEDs and it resolved my issue completely.
+
+Bad serial output (nothing changing on scoring machine):
+```
+11:30:44.289 -> The message in HEX is: FF,0,0,0,0,0,0,0,10,0,
+11:30:45.554 -> The message in HEX is: FF,7F,0,0,0,0,0,0,0,0,
+11:30:45.882 -> The message in HEX is: FF,FD,0,0,0,0,0,0,0,0,
+```
+Good serial output (nothing changing on scoring machine):
+```
+11:45:23.587 -> The message in HEX is: FF,0,0,0,0,0,0,0,30,0,
+```
---
+++ Wemos_Favero_Serial_Parser_Rev_1/Wemos_Favero_Serial_Parser_Rev_1.ino
@@ -0,0 +1,185 @@
+#include <FastLED.h>
+#define MY_NAME "REPEATER"
+
+const unsigned int MAX_MESSAGE_LENGTH = 10;
+const unsigned int MAX_SERIAL_BUFFER_BYTES = 512;
+const char STARTING_BYTE = 255;
+
+const int FOTL_DATA = 4; // D2
+const int FOTR_DATA = 5; // D1
+const int NUM_LEDS = 300; // Number of LEDs in your LED strip/matrix
+unsigned int COUNTER = 0;
+bool MACHINE_OFF = true;
+
+CRGB left[NUM_LEDS]; // FOTL LED array
+CRGB right[NUM_LEDS]; // FOTR LED array
+
+struct __attribute__((packed)) dataPacket {
+  int unsigned Right_Score;
+  int unsigned Left_Score;
+  int unsigned Seconds_Remaining;
+  int unsigned Minutes_Remaining;
+  bool Green_Light;
+  bool Red_Light;
+  bool White_Green_Light;
+  bool White_Red_Light;
+  bool Yellow_Green_Light;
+  bool Yellow_Red_Light;
+  bool Yellow_Card_Green;
+  bool Yellow_Card_Red;
+  bool Red_Card_Green;
+  bool Red_Card_Red;
+  bool Priority_Left;
+  bool Priority_Right;
+};
+
+// Shows if new data is available for display
+bool new_data = false;
+
+// Initializes Message_Position
+unsigned int message_pos = 0;
+
+void setup() {
+
+  FastLED.addLeds<WS2812, FOTL_DATA, GRB>(left, NUM_LEDS);
+  FastLED.addLeds<WS2812, FOTR_DATA, GRB>(right, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 2000); 
+
+  Serial.setRxBufferSize(1024);
+  Serial.begin(2400);  // initialize serial port
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.print("Initializing...");
+  Serial.println(MY_NAME);
+  Serial.println("Initialized.");
+}
+
+void loop() {
+
+  EVERY_N_MILLISECONDS(200) {
+    wdt_reset();
+  }
+
+  dataPacket packet;
+
+  //  Check to see if anything is available in the serial receive buffer
+  while (Serial.available() > MAX_MESSAGE_LENGTH) {
+
+    // Create a place to hold the incoming message
+    COUNTER = 0;
+    MACHINE_OFF = false;
+    static char message[MAX_MESSAGE_LENGTH];
+    static char prev_message[MAX_MESSAGE_LENGTH];
+
+    char inByte = Serial.read();
+
+    // Message coming in (check if starting character)
+    if (inByte == STARTING_BYTE) {
+      // Resets message position
+      message_pos = 0;
+      // Stores the Byte in the message position
+      message[message_pos] = inByte;
+      //increments message position
+      message_pos++;
+    } else if (message_pos < (MAX_MESSAGE_LENGTH - 1)) {
+      // Stores the Byte in the message position
+      message[message_pos] = inByte;
+      //increments message position
+      message_pos++;
+    } else if (message_pos == (MAX_MESSAGE_LENGTH - 1)) {
+
+      // Prints the Message if different from previous
+      // Excudes the internal use byte in position 7 or check sum.
+      if (message[1] != prev_message[1] or message[2] != prev_message[2] or message[3] != prev_message[3] or message[4] != prev_message[4] or message[5] != prev_message[5] or message[6] != prev_message[6] or message[8] != prev_message[8]) {
+        // Sets New Data to True
+        new_data = true;
+
+        Serial.print("The message in HEX is: ");
+        for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
+          Serial.print(message[i], HEX);
+          Serial.print(",");
+        }
+        Serial.print('\n');
+
+        // Assigns values from message to packet
+        packet.Green_Light = bitRead(message[5], 3);
+        packet.Red_Light = bitRead(message[5], 2);
+        packet.White_Green_Light = bitRead(message[5], 1);
+        packet.White_Red_Light = bitRead(message[5], 0);
+        packet.Yellow_Green_Light = bitRead(message[5], 4);
+        packet.Yellow_Red_Light = bitRead(message[5], 5);
+
+        // Stuff not needed for repeaters
+        packet.Yellow_Card_Green = bitRead(message[8], 2);
+        packet.Yellow_Card_Red = bitRead(message[8], 3);
+        packet.Red_Card_Green = bitRead(message[8], 0);
+        packet.Red_Card_Red = bitRead(message[8], 1);
+
+        packet.Priority_Right = bitRead(message[6], 2);
+        packet.Priority_Left = bitRead(message[6], 3);
+
+        // Stores the Score and Time
+        packet.Right_Score = hex_string_to_int(message[1]);
+        packet.Left_Score = hex_string_to_int(message[2]);
+        packet.Seconds_Remaining = hex_string_to_int(message[3]);
+        packet.Minutes_Remaining = hex_string_to_int(message[4]);
+
+        // Sets Previous Message to Current Message
+        for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
+          prev_message[i] = message[i];
+        }
+
+        // Resets Message Position
+        message_pos = 0;
+
+        // Clears the Serial Buffer if more than Max Buffer Bytes bytes in the buffer
+        if (Serial.available() > MAX_SERIAL_BUFFER_BYTES) {
+          Serial.println("...............Clearing the Serial Buffer...............");
+          while (Serial.available() > 0) {
+            char inByte = Serial.read();
+          }
+        }
+        // Right fencer lights
+        if (packet.Green_Light == 1) {
+          fill_solid(right, NUM_LEDS, CRGB::Green); // On target right
+          Serial.println("FOTR ON TARGET");
+        } else if (packet.White_Green_Light == 1) {
+          fill_solid(right, NUM_LEDS, CRGB::White); // Off target right
+          Serial.println("FOTR OFF TARGET");
+        } else {
+          fill_solid(right, NUM_LEDS, CRGB::Black); // No light on right
+          Serial.println("FOTR NO LIGHT");
+        }
+        // Left fencer lights
+        if (packet.Red_Light == 1) {
+          fill_solid(left, NUM_LEDS, CRGB::Red);
+          Serial.println("FOTL ON TARGET");
+        } else if (packet.White_Red_Light == 1) {
+          fill_solid(left, NUM_LEDS, CRGB::White);
+          Serial.println("FOTL OFF TARGET");
+        } else {
+          fill_solid(left, NUM_LEDS, CRGB::Black);
+          Serial.println("FOTL NO LIGHT");
+        }
+        FastLED.show(); // Send the changes to the LEDs
+      }
+    } else {
+      Serial.println("Unexpected Message Position, Reseting to zero.");
+      message_pos = 0;
+    }
+  }
+  COUNTER++;
+  if (COUNTER > 10000) { // turn off the lights if 10,000 loops have passed and haven't gotten any serial connection.
+    if (MACHINE_OFF == false) { // only turn the lights off the lights once if 10,000 loops have passed
+      MACHINE_OFF = true;
+      fill_solid(left, NUM_LEDS, CRGB::Black);
+      fill_solid(right, NUM_LEDS, CRGB::Black);
+      Serial.println("machine off");
+      FastLED.show();
+    }
+    COUNTER = 0;
+    MACHINE_OFF = true;
+  }
+}
